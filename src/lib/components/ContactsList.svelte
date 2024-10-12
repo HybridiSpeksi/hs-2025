0dc3183cff8830c087e96aa35e15881f04ee526c
Lauri Maila <laurikmaila@gmail.com>
2024-10-12 23:00:15 +0300

    Multiple small styling changes

--- src/lib/components/ContactsList.svelte
+++ src/lib/components/ContactsList.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	import type { ContactItem } from '$lib/types/content'
-	export let contacts: ContactItem
+	import type { ContactItem } from '$lib/types/content';
+	export let contacts: ContactItem;
 </script>
 
-<div>
-	<p><strong>Sähköposti: </strong>{contacts?.email}</p>
-	<p><strong>Y-tunnus: </strong>{contacts?.yTunnus}</p>
-	<p><strong>Tilinumero: </strong>{contacts?.IBAN}</p>
-	<p><strong>Osoite: </strong>{contacts?.address}</p>
+<div class="contacts-list">
+	<p class="contact-item"><strong>Sähköposti: </strong>{contacts?.email}</p>
+	<p class="contact-item"><strong>Y-tunnus: </strong>{contacts?.yTunnus}</p>
+	<p class="contact-item"><strong>Tilinumero: </strong>{contacts?.IBAN}</p>
+	<p class="contact-item"><strong>Osoite: </strong>{contacts?.address}</p>
 </div>
+
+<style>
+	.contacts-list {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		@media screen and (max-width: 768px) {
+			gap: 0.3rem;
+		}
+	}
+
+	.contact-item {
+		margin: 0;
+	}
+</style>
