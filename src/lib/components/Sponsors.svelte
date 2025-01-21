1e4d963e5ded8366aaf9c3a6553e8c5ef0e81727
Lauri Maila <laurikmaila@gmail.com>
2025-01-22 00:55:32 +0200

    Fixed sponsors component

--- src/lib/components/Sponsors.svelte
+++ src/lib/components/Sponsors.svelte
@@ -24,7 +24,7 @@
     }
 </script>
 
-<div class="all-sponsors-container">
+<div class="sponsors-component">
     {#if hasMainSponsors()}
         <h2 class="global-center main-sponsor-heading">{getMainSponsorHeading()}</h2>
         <div class="main-sponsors-container sponsors-container">
@@ -77,20 +77,8 @@
         text-align: center;
         width: 100%;
     }
-    .sponsors-container {
-        display: flex;
-        width: 100%;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
-        padding-bottom: 25px;
-        z-index: 1;
-        img {
-            z-index: 10;
-        }
-    }
 
-    .all-sponsors-container {
+    .sponsors-component {
         position: relative;
         display: flex;
         width: 100%;
@@ -100,7 +88,8 @@
         padding: 50px 0;
         overflow: hidden;
     }
-    .all-sponsors-container::before {
+
+    .sponsors-component::before {
         content: '';
         position: absolute;
         top: 0;
@@ -114,44 +103,62 @@
         opacity: 0.05;
     }
 
+    .sponsors-container {
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        padding-bottom: 25px;
+        z-index: 1;
+        img {
+            z-index: 10;
+        }
+    }
+
     .main-sponsor-heading {
         margin-bottom: 25px;
         align-self: center;
     }
 
-    .sponsor-logo {
-        max-width: 150px;
-        padding: 10px;
-        &:hover {
-            transform: scale(1.2);
-        }
-    }
-
-    .main-sponsor-logo-container {
+    .sponsor-logo-container {
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 23rem;
+        width: 19rem;
 
         @media (max-width: 767px) {
             margin: 0 4rem;
         }
     }
-    .main-sponsor-logo {
-        max-width: 400px;
-        width: 100%;
-        &:hover {
-            transform: scale(1.1);
-        }
+
+    .main-sponsor-logo-container {
+        @extend .sponsor-logo-container;
+        width: 23rem;
     }
 
-    .sponsor-logo,
-    .main-sponsor-logo {
+    .sponsor-logo {
+        max-width: 150px;
+        padding: 10px;
         height: auto;
         max-width: 100%;
         border: solid transparent 5px;
         margin: 0;
         cursor: pointer;
         transition: transform 0.3s ease;
+
+        &:hover {
+            transform: scale(1.2);
+        }
+    }
+
+    .main-sponsor-logo {
+        @extend .sponsor-logo;
+        max-width: 400px;
+        width: 100%;
+
+        &:hover {
+            transform: scale(1.1);
+        }
     }
 </style>
