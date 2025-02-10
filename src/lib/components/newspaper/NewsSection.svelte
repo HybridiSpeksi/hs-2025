4dbd33ff2f45cac19d217954b4554811e717c094
tuplawee7 <92923056+williamlempinen@users.noreply.github.com>
2025-02-10 15:22:50 +0200

    Newspaper background (#4)
    basic components of newspaper for testing and development

---
+++ src/lib/components/newspaper/NewsSection.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    export let hasTitleOf: string;
+</script>
+
+<article class="article">
+    {#if hasTitleOf}
+        <h3 class="title">{hasTitleOf}</h3>
+        <hr class="line" />
+    {/if}
+    <div class="news-section">
+        <slot />
+    </div>
+</article>
+
+<style lang="scss">
+    .article {
+        margin: 1rem;
+    }
+
+    .title {
+        font-size: 1.5rem;
+        font-weight: bold;
+        font-family: 'IM Fell English', serif;
+        padding: 0;
+        margin: 0;
+    }
+
+    .line {
+        width: 100%;
+        height: 1px;
+        background-color: black;
+        margin: 0;
+        padding: 0;
+    }
+
+    .news-section {
+        display: flex;
+        flex-direction: column;
+        gap: 0.2rem;
+        padding: 0.5rem;
+    }
+</style>
---
+++ src/lib/components/newspaper/NewspaperTitle.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let title: string;
+</script>
+
+<div class="newspaper-title">
+    <h1>{title}</h1>
+
+    <hr class="line" />
+    <hr class="line" />
+    <hr class="line" />
+</div>
+
+<style lang="scss">
+    .newspaper-title {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        font-family: 'IM Fell English', serif;
+        padding: 1rem 0;
+
+        h1 {
+            margin: 0;
+            padding: 0;
+            font-size: 2.5rem;
+            text-transform: uppercase;
+            letter-spacing: 0.1rem;
+        }
+    }
+
+    .line {
+        width: 100%;
+        height: 1px;
+        background-color: black;
+        margin: 0;
+        padding: 0;
+    }
+</style>
