eb8fc2286934d6c6f46f3bb9a7fe7f34ec233974
Jaakko Ekqvist <ekqvistjaakko@gmail.com>
2026-03-13 17:01:37 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -95,7 +95,7 @@ Ja tästä trailer!
 
 			<Video 
 				videoURL="https://www.youtube.com/embed/BwOgh20Odao"
-				code="/src/lib/images/2026/traileri.jpg" />
+				code="$lib/images/2026/traileri.jpg" />
 	</div>
 </div>
 
